108de888ff7f96cbbd9ea2e3a491e40561b901ab
samuel <71820869+tzl122@users.noreply.github.com>
2025-10-29 08:27:12 +0630

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sqlite3,string,random
+import sqlite3,string,random,os
 from datetime import datetime
 from flask import Flask,render_template,request,redirect,url_for
 
@@ -84,5 +84,6 @@ def admin():
 
     return render_template("admin.html",login=login,confs=data)
 
-if __name__=="__main__":
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
